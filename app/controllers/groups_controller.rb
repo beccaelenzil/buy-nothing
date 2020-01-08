8699904ffa0a85177c57bd276fc184343f737cb7
beccaelenzil <beccaelenzil@gmail.com>
2020-01-08 11:10:50 -0800

    group CRUD actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
-  GROUPS = ['Madison Valley', 'Madison Park', 'Capitol Hill']
+
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
+
   def index
-    @groups = GROUPS
+    @groups = Group.all
   end
 
   def show
-    group_id = params[:id].to_i
-    @group = GROUPS[group_id]
     if @group.nil?
       head :not_found
       return
@@ -14,9 +14,46 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
-  def create
+  def create  
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_path(@group.id)
+      return
+    else 
+      render :new 
+      return
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path(@group.id)
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @group.destroy
+    
+    redirect_to books_path
+    return
+  end
+
+  def group_params
+    return params.require(:group).permit(:name, :description, :bounds, :map, :city, :state, :country)
+  end
+
+  def find_group
+    @group = Group.find_by(id: params[:id])
   end
 
 end
