5faaf5ec133a1f9224e0aeea4b69a421bf597c81
beccaelenzil <beccaelenzil@gmail.com>
2020-01-13 08:47:05 -0800

    fixture updates

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 describe Group do
+
   describe 'relations' do
     it "has members" do
       group = groups(:mcgilvra)
@@ -8,8 +9,15 @@ describe Group do
     end
     it "can add members" do
       group = Group.create(name: "madrona")
-      member = Member.create(email: "me@ada.com", group_id: group.id)
+      member = Member.create(email: "me@ada.com")
+      relationship = Relationship.create(group_id: group.id, member_id: member.id, status: "regular")
       group.members.first.id = member.id
     end
+  end
+
+  #custom methods
+  describe "owner" do
+    group = groups(:mcgilvra)
+    group.owner.must_equal members(:becca)
   end
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -2,25 +2,27 @@ require "test_helper"
 require 'pry'
 
 describe Member do
+
   describe 'relations' do
     it "belongs to a group" do
       member = members(:becca)
-      member.group.must_equal groups(:mcgilvra)
+      member.group.must_include groups(:mcgilvra)
+      member.group.must_include groups(:madrona)
     end
     it "can set the group" do
       member = Member.new(email: "me@ada.com")
       member.group = groups(:mcgilvra)
-      member.group_id.must_equal groups(:mcgilvra).id
+      member.groups.must_include groups(:mcgilvra)
     end
-    it "has items" do
-      member = members(:becca)
-      member.items.length.must_equal 4
-    end
-    it "can add items" do
-      group = groups(:mcgilvra)
-      member = Member.create!(email: "ve@ada.com", group_id: group.id)
-      item = Item.create(title: "swing", member_id: member.id)
-      member.items.first.id.must_equal item.id
+    #it "has items" do
+    #  member = members(:becca)
+    #  member.items.length.must_equal 4
+    #end
+    #it "can add items" do
+    #  group = groups(:mcgilvra)
+    #  member = Member.create!(email: "ve@ada.com",# group_id: group.id)
+    #  item = Item.create(title: "swing", member_id: member.id)
+    #  member.items.first.id.must_equal item.id
     end
   end
 end
